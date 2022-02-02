c3e780b7620590f9fd58cb6acf451ad31d357907
Daniel Adams <danstanswinxp@gmail.com>
2022-02-02 22:09:18 +0000

    Portfolio project template

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let mustacheExpress = require('mustache-express');
 let bodyParser = require('body-parser');
+const fs = require('fs');
 
 let app = express();
 
@@ -20,7 +21,16 @@ app.get ('/qualifications', function(req,res) {
 });
 
 app.get ('/portfolio', function(req,res) {
-    res.render('portfolio', {pageTitle: "Portfolio"});
+    const projectsFolder = './projects/';
+
+    var projects = [];
+
+    fs.readdirSync(projectsFolder).forEach(file => {
+        const data = fs.readFileSync("./projects/" + file, 'utf8');
+        projects.push(JSON.parse(data));
+    })
+
+    res.render('portfolio', {pageTitle: "Portfolio", projects: projects});
 });
 
 app.listen(5014,function() {
